95ebdd92295ee05c782486f9a0132f65c7cb70b3
Janaki-5 <jjananu676@gmail.com>
2025-08-08 10:20:56 +0530

    Add files via upload

---
+++ firebox.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class firebox {
+	public firebox() {
+		float browserVersion=100.2f;
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("browserVersion"+browserVersion);
+		System.out.println("isVisible"+isVisible);
+		System.out.println("reLeasYear"+releaseYear);
+		System.out.println("browserLogo"+browserLogo);
+	}
+	public static void main(String[] args) {
+		firebox obj=new firebox();
+	}
+	
+}
+
