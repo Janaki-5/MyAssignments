901c7226d94a4e0094470e3f84052d52b45d2a4a
Janaki-5 <jjananu676@gmail.com>
2025-08-09 02:48:42 -0700

    Update MissingElement.java

--- MissingElement.java
+++ MissingElement.java
@@ -1,19 +1,27 @@
 package PositiveAndNegative;
 
 public class MissingElement {
-	    public static void main(String[] args) {
-	        int[] arr1 = {1, 2, 3, 4, 5};
-	        int[] arr2 = {3, 4, 5, 6, 7};
+	  public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 6, 7}; // Missing: 3, 5
+        int n = 7; // range 1 to n
 
-	        System.out.print("Intersection: ");
-	        for (int i = 0; i < arr1.length; i++) {
-	            for (int j = 0; j < arr2.length; j++) {
-	                if (arr1[i] == arr2[j]) {
-	                    System.out.print(arr1[i] + " ");
-	                }
-	            }
-	        }
+        System.out.print("Missing elements: ");
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
 	    }
 	}
+
 
 
