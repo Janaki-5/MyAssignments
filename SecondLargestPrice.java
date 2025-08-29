f6a761e2911a9f01883f4f2666e0727465549dc1
Janaki-5 <jjananu676@gmail.com>
2025-08-29 07:15:52 -0700

    Update SecondLargestPrice.java

--- SecondLargestPrice.java
+++ SecondLargestPrice.java
@@ -1,80 +1,51 @@
 package largestpriceprint;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SecondLargestPrice {
-	public static void main(String[] args) throws IOException {
+public class PriceExtractor {
+    public static void main(String[] args) throws IOException {
+        // Setup ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-		        
-		        
-		        ChromeDriver driver = new ChromeDriver();
+        // Open website
+        driver.get("https://www.amazon.in/s?k=mobiles"); // Example URL
 
-		        try {
-		          
-		            driver.get("https://secudemo.com/v1/");
-		            List<WebElement> priceElements = driver.findElements(By.cssSelector(".price"));
+        // Collect all price elements (change XPath as per website structure)
+        List<WebElement> priceElements = driver.findElements(By.xpath("//span[@class='a-price-whole']"));
 
-		            // Store all parsed prices
-		           List<Double> prices = new ArrayList<>();
+        List<Integer> prices = new ArrayList<>();
 
-		            // Print all prices
-		            System.out.println("All Prices:");
-		            for (WebElement element : priceElements) {
-		                String priceText = element.getText().replaceAll("[^\\d.]", ""); // Remove currency symbol
-		                if (!priceText.isEmpty()) {
-		                    double price = Double.parseDouble(priceText);
-		                    prices.add(price);
-		                    System.out.println(price);
-		                }
-		            }
+        for (WebElement priceElement : priceElements) {
+            String priceText = priceElement.getText().replace(",", "").trim();
+            if (!priceText.isEmpty()) {
+                prices.add(Integer.parseInt(priceText));
+            }
+        }
 
-		            // Print the largest price
-		            if (!prices.isEmpty()) {
-		                double maxPrice = Collections.max(prices);
-		                System.out.println("\nLargest Price: " + maxPrice);
-		            }
+        // Print all prices
+        System.out.println("All Prices: " + prices);
 
-		           
-		            System.out.println("\nScreenshot saved as 'screenshot.png'");
-		        } finally {
-		            // Close browser
-		            driver.quit();
-		        }
-		       
-		    }
+        // Find largest price
+        int maxPrice = Collections.max(prices);
+        System.out.println("Largest Price: " + maxPrice);
 
-	private TakesScreenshot driver;
-	{
-		
-		File source = driver.getScreenshotAs(OutputType.FILE);
+        // Take Screenshot
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("screenshot.png"));
 
-		File destination = new File("./janaki/product.png");
-
-		try {
-			FileUtils.copyFile(source, destination);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	
-	}
+        // Close browser
+        driver.quit();
+    }
 }
-
-
-
-
-	
-
-
